933ea491d72d191657ea916e30fa63bae74e0290
Quentin <quentin.boileau@gmail.com>
2025-02-02 13:21:55 +0000

    fix infinit recusion

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,8 +1,8 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
 # You can build them using 'nix build .#example'
-{pkgs, ...} :
-{
-    import = [
+
+pkgs: {
+  import = [
       ./shells
       ./base-tools.nix
 
