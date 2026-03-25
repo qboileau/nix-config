1d0a2cf05711a2d34028bae3a4dea5ff728df52e
Quentin Boileau <quentin.boileau@gmail.com>
2026-03-25 11:25:11 +0100

    Update dev template

--- dev-env-templates/flake.nix
+++ dev-env-templates/flake.nix
@@ -1,14 +1,28 @@
 {
   description = "my project description";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem
       (system:
         let pkgs = nixpkgs.legacyPackages.${system}; in
         {
-          devShells.default = import ./shell.nix { inherit pkgs; };
+          devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            git
+          ];
+
+          shellHook = ''
+            export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [
+            ]}:$LD_LIBRARY_PATH"
+
+            echo "Development environment loaded"
+          '';
+        };
         }
       );
 }
