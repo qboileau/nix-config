016a12b876121f3fe978b397873ed49a2bfe4e30
qboileau <quentin.boileau@gmail.com>
2026-05-14 22:30:38 +0200

    Fix boot partition permissions

--- nixos/vm/disks.nix
+++ nixos/vm/disks.nix
@@ -20,7 +20,7 @@
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
-                mountOptions = [ "fmask=0022" "dmask=0022"  ];
+                mountOptions = [ "fmask=0077" "dmask=0022" ];
               };
             };
             luks = {
