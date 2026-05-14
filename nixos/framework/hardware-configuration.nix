668b03f222f5c14e098e891f3a19dcce530bf990
Quentin Boileau <quentin.boileau@gmail.com>
2026-05-15 00:42:47 +0200

    Remove unnessary fprintd config

--- nixos/framework/hardware-configuration.nix
+++ nixos/framework/hardware-configuration.nix
@@ -80,10 +80,6 @@
   services.fprintd.enable = true;
   services.fprintd.tod.enable = true;
   services.fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix;
-  systemd.services.fprintd = {
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig.Type = "simple";
-  };
 
 
   environment.systemPackages = with pkgs; [
