3b77951addd238400c67bab4adfe1b3e26feda2a
Quentin Boileau <quentin.boileau@gmail.com>
2026-05-15 18:58:17 +0200

    Fix fprind config on framework

--- nixos/framework-amd/hardware-configuration.nix
+++ nixos/framework-amd/hardware-configuration.nix
@@ -78,9 +78,6 @@
 
   # Fprint
   services.fprintd.enable = true;
-  services.fprintd.tod.enable = true;
-  services.fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix;
-
 
   environment.systemPackages = with pkgs; [
    framework-tool
