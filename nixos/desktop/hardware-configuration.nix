a4aeeb4005dd816ce57689e9a319f5351407ed13
qboileau <quentin.boileau@gmail.com>
2026-08-23 22:26:12 +0200

    desktop: switch to unstable channel for mesa

--- nixos/desktop/hardware-configuration.nix
+++ nixos/desktop/hardware-configuration.nix
@@ -77,8 +77,8 @@ in {
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
-    # package = pkgs.unstable.mesa;
-    # package32 = pkgs.unstable.pkgsi686Linux.mesa;
+    package = pkgs.unstable.mesa;
+    package32 = pkgs.unstable.pkgsi686Linux.mesa;
   };
 
   hardware.bluetooth = {
